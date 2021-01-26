4eb01f04ceb79a5e38460bc5375a6c0f9b418717
Insan <insan.putranda@gmail.com>
2021-01-26 12:12:50 +0700

    Added damage health and projectile collision

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var damage = collision.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damage);
+    }
+
+    private void ProcessHit(DamageDealer damage)
+    {
+        health -= damage.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
