cb4ac8c7fa56fbe550295ef2a45b77e9c65c95e5
Insan <insan.putranda@gmail.com>
2021-01-26 22:10:13 +0700

    Added null handler collision player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -77,7 +77,14 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        health -= collision.gameObject.GetComponent<DamageDealer>().GetDamage();
+        var damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) return;
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
         if (health <= 0)
         {
             Destroy(gameObject);
